dc4fb47c4f2585db3727aff62addb39ecd17845e
Emilio Degiovanni <emilio.degiovanni@softvision.com>
2021-10-05 22:48:50 -0300

    tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,35 +1,13 @@
-import { TestBed, async } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
-    }).compileComponents();
-  }));
+  let component: AppComponent;
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+  beforeEach(() => {
+    component = new AppComponent();
   });
 
-  it(`should have as title 'ghactions-test'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('ghactions-test');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('ghactions-test app is running!');
+  it('should create', () => {
+    expect(component).toBeTruthy();
   });
 });
--- src/app/components/collatz/collatz.component.spec.ts
+++ src/app/components/collatz/collatz.component.spec.ts
@@ -1,25 +1,43 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { CollatzComponent } from './collatz.component';
 
 describe('CollatzComponent', () => {
   let component: CollatzComponent;
-  let fixture: ComponentFixture<CollatzComponent>;
-
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ CollatzComponent ]
-    })
-    .compileComponents();
-  }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(CollatzComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    component = new CollatzComponent();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('secuency', () => {
+    it('should NOT get values with 0', () => {
+      component.initialValue = 0;
+
+      expect(component.secuency.length).toEqual(0);
+    });
+
+    it('should get only 1 for 1', () => {
+      component.initialValue = 1;
+
+      expect(component.secuency.length).toEqual(1);
+      expect(component.secuency).toEqual([1]);
+    });
+
+    it('should get same result for negatives input', () => {
+      component.initialValue = 10;
+      const expected = component.secuency;
+
+      component.initialValue = -10;
+      expect(component.secuency).toEqual(expected);
+    });
+
+    it('should last element equal to 1 with huge input', () => {
+      component.initialValue = 99999;
+
+      expect(component.secuency.pop()).toEqual(1);
+    });
   });
 });
